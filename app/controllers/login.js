5c9821d42d873e383a34de6a788cee61e93edc12
lisssb <liss.sb@gmail.com>
2015-07-03 09:41:53 +0200

    login templete improved

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,17 +1,42 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  timeout : null,
+  //isSlowConnection: false,
   actions : {
-  login : function(){
-    $.post("http://88.12.28.199/", {
-      username: this.get("username"),
-      password: this.get("password")
-    }).then(function() {
-      //this.transitionToRoute('index');
-    }, function() {
-      //this.transitionToRoute('index');
-      this.set("loginFailed", true);
-    }.bind(this));
+    login : function(){
+      this.setProperties({
+        loginFailed: false,
+        isProcessing: true
+      });
+
+      //this.set("timeout", setTimeout(this.slowConnection.bind(this), 5000));
+      var request = Ember.$.post("http://192.168.1.45:3000/test", { "credentials" :{
+        username: this.getProperties("username"),
+        password: this.getProperties("password")
+      }});
+      request.then(this.success.bind(this), this.failure.bind(this));
+    }
+  },
+  failure: function() {
+    this.reset();
+    this.setProperties({
+      username : '',
+      password : ''
+    });
+    this.set("loginFailed", true);
+  },
+  success: function() {
+    this.transitionToRoute('index');
+  },
+  /*slowConnection: function() {
+    this.set("isSlowConnection", true);
+  },*/
+  reset: function() {
+    clearTimeout(this.get("timeout"));
+    this.setProperties({
+      isProcessing: false,
+      //isSlowConnection: false
+    });
   }
-}
 });
